5d84bfc806177b92c66081c288d13ba2a0d6f215
Derek Lewandowski <derekclewandowski@gmail.com>
2018-08-31 14:08:50 -0500

    adding basic index route

---
+++ index.js
@@ -0,0 +1,61 @@
+// Environment Variables
+require("dotenv").config();
+
+// module imports
+const compression = require("compression");
+const express = require("express");
+const winston = require("winston");
+const expressWinston = require("express-winston");
+const helmet = require("helmet");
+const routes = require("./routes");
+
+// module configuration
+const app = express();
+const port = process.env.PORT;
+
+// winston request logging
+// middleware to log your HTTP requests
+app.use(
+  expressWinston.logger({
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true,
+      }),
+    ],
+    meta: true,
+    msg: "HTTP {{req.method}} {{req.url}}",
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function(req, res) {
+      return false;
+    },
+  }),
+);
+
+// app configuration
+app.use(compression());
+app.use(helmet());
+
+// routes
+app.use("/", routes);
+
+/**
+ * winston error logging
+ * middleware that log the errors of the pipeline.
+ */
+app.use(
+  expressWinston.errorLogger({
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true,
+      }),
+    ],
+  }),
+);
+
+// starting the server
+app.listen(port);
+
+module.exports = app;
